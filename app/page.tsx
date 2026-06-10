73f0b2d834f4be264ed9468f35764174b3430c5e
lekhaams-hub <lekhaams@gmail.com>
2026-06-10 18:11:39 +0530

    Add main layout structure to page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+<main>
+        <Hero />
+        <GamifiedBanner />
+        
+        {/* Unified Overarching Method + Crash Course Journey */}
+        <MethodTimeline />
+        
+        <CurriculumGrid />
+        
+        {/* Restored with your custom text and image layout */}
+        <AboutFounder />
+        
+        {/* 1. Moved Testimonials up */}
+        <Testimonials /> 
+        
+        {/* 2. LMSGate (the form from image_456404.png) now sits underneath */}
+        <LMSGate />
+        
+        <FAQ />
+        <ContactFooter />
+      </main>
